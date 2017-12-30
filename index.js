334218acc0970bb908164419b2c49b98a813d0c6
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-12-30 22:23:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function scuberGreetingForFeet(distance) {
 if (distance <= 400) {
    'This one is on me!'
 }
-else if (distance > 400 && distance <- 2500)
+else if (distance > 2000 && distance <- 2500)
 { 'I will gladly take your 30 bucks'}
 }
