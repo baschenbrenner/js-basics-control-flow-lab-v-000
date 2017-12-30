d59ef296b811eb85b3d4a7f6ff028197dc7a4ec4
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-12-30 22:47:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-
-function ternaryCheckCity(city)
-{
-if city === 'NYC' {
-  return 'Ok, sounds good.';
-}
-else  {
-  return 'No go.';
-}
-}
+var scuberGreetingForFeet;
+// function scuberGreetingForFeet(distance) {
+// if (distance <= 400) {
+// return 'This one is on me!';
+// }
+// else if (distance > 2000 && distance <= 2500) {
+//  return 'I will gladly take your thirty bucks.';
+// }
+// else if (distance >2500) {
+// return 'No can do.';
+// }
+// }
+var ternaryCheckCity;
+// function ternaryCheckCity(city)
+// {
+// if city === 'NYC' {
+//   return 'Ok, sounds good.';
+// }
+// else  {
+//   return 'No go.';
+// }
+// }
