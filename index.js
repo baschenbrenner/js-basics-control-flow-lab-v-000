b4cf334ed2023d7aca90052561fc993ca8fce05f
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-12-30 22:32:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,18 +1,5 @@
 
 
-function scuberGreetingForFeet(distance) {
-if (distance <= 400) {
-  return 'This one is on me!';
-}
-else if (distance > 2000 && distance <= 2500) {
-   return 'I will gladly take your thirty bucks.';
-}
-else if (distance >2500) {
-  return 'No can do.';
-}
-}
-
-
 function ternaryCheckCity(city)
 {
 if city === 'NYC' {
